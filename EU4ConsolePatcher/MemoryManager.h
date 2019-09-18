f535ff8974c55319f085fb1604276ba9e2c0819e
Daniel Gwozdz <d.gwozdz@live.de>
2019-09-18 20:05:00 +0200

    Add support vor v1.29.0 update
    Updated memory signatures, updated signature scanning mechanism to prevent
    early exit after \0 byte, added minor solution updates because of missing win sdk.

--- EU4ConsolePatcher/MemoryManager.h
+++ EU4ConsolePatcher/MemoryManager.h
@@ -7,10 +7,10 @@ public:
 	MemoryManager(const DWORD& processId);
 	~MemoryManager();
 	bool Patch(const patchInfo_t& patchInfo);
-	bool FindPattern(const wchar_t* pattern, const BYTE* address, const SIZE_T& size, DWORD& offset);
+	bool FindPattern(const wchar_t* pattern, const wchar_t* signature, const BYTE* address, const SIZE_T& size, DWORD& offset);
 private:
 	DWORD processId;
 
-	bool CompareData(const BYTE* data, const wchar_t* pattern);
+	bool CompareData(const BYTE* data, const wchar_t* pattern, const wchar_t* signature);
 };
 
